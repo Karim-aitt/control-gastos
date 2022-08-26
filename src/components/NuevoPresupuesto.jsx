a8849b72fa96030fb7cd104c5972911e83189159
Karim <karim.gonzalez.ait@gmail.com>
2022-08-26 22:13:17 +0200

    update minor fixes

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,7 +1,11 @@
 import {useState} from "react";
 import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({ 
+    presupuesto, 
+    setPresupuesto,
+    setIsValidPresupuesto
+}) => {
 
     const [mensaje, setMensaje] = useState("")
 
@@ -14,6 +18,7 @@ const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
         }
 
         setMensaje("")
+        setIsValidPresupuesto(true)
     }
 
 	return (
